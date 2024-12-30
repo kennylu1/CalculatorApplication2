ac08874b6ac66d5640aac032604c630b22899fc4
kennylu <a29037651@yahoo.com.tw>
2024-12-30 09:19:41 +0800

    不太懂流程需要問

--- HelpAttribute/Program.cs
+++ HelpAttribute/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace HelpAttribute
+﻿using System;
+
+[AttributeUsage(AttributeTargets.All)]
+
+public class HelpAttribute : System.Attribute
+
 {
-    internal class Program
+    public readonly string Url;
+    private string topic = string.Empty;
+
+    public string Topic
+    {
+        get
+
+        { return topic; }
+        set { topic = value; }
+
+
+    }
+
+    public HelpAttribute(string url)
+    {
+        this.Url = url;
+    }
+
+}
+[HelpAttribute("Information on the class MyClass")]
+
+class MyClass { }
+
+namespace AttributeApp1
+{
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            System.Reflection.MemberInfo info = typeof(MyClass);
+            object[] attributes = info.GetCustomAttributes(true);
+            for (int i = 0; i < attributes.Length; i++)
+            {
+                Console.WriteLine(attributes[i]);
+            }
+            Console.ReadKey();
+
         }
     }
 }
