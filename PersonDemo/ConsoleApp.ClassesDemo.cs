438293697fdf42485edfb97a91ed71053c61038d
kennylu <a29037651@yahoo.com.tw>
2025-01-02 11:58:58 +0800

    類別及物件

---
+++ PersonDemo/ConsoleApp.ClassesDemo.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp.ClassesDemo.Classes.PersonDemo;
+
+Console.WriteLine("************* - Classes and Objects - *************");
+
+/* Define an object of type Person. this will be null by default. Without the new instance, it will give a null exception if it is used.
+*/
+Person person;
+// person = new Person(); // Can be initialized at another point of the code. 
+
+// It is best to define and declare a new object. 
+Person baby = new Person();
+
+// Using the . operator, we can access public properties and methods.
+baby.LastName = "Thompson";
+baby.FirstName = "Theresa";
+baby.DateofBirth = new DateOnly(2023, 10, 29);
+baby.PrintFullName();
+baby.GenerateTaxNumber();
+var taxNumber = baby.GetTaxNumber();
+baby.PrintInitials();
+Console.WriteLine(taxNumber);
+
---
+++ PersonDemo/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Define a class
+using System.Security.Cryptography;
+
+namespace ConsoleApp.ClassesDemo.Classes.PersonDemo;
+
+public  class Person
+{
+    public Person()
+    {
+
+    }
+    public Person(string firstName, string lastName, DateOnly dbo)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DateofBirth = dbo;
+    }
+
+    //public Person(string firstName, string lastName, string taxNumber)
+    //{
+    //    FirstName = firstName;
+    //    LastName = lastName;
+    //    if (string.IsNullOrEmpty(taxNumber))
+    //    {
+    //        TaxNumber = GenerateTaxNumber();
+    //    }
+    //    else
+    //    {
+    //        TaxNumber = taxNumber;
+    //    }
+    //}
+
+
+
+    // Properties/Data Members
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateofBirth { get; set; }
+    //public string TaxNumber { get; private set; }
+
+    // Field member
+    private string _taxNumber;
+
+
+    public void PrintFullName()
+    {
+        var type = GetType().Name;
+        Console.WriteLine($"{type} name: {FirstName} {LastName}");
+    }
+
+    public void PrintInitials()
+    {
+        var first = FirstName[0];
+        var lastInital= LastName[0];
+        Console.WriteLine(first + " " + lastInital);
+    }
+    public void GenerateTaxNumber()
+    {
+        _taxNumber = RandomNumberGenerator.GetInt32(100000,9999999).ToString();
+    }
+    public string GetTaxNumber()
+    {
+        return _taxNumber;
+    }
+
+    public int GetAge()
+    {
+        var age=DateTime.Now.Year-DateofBirth.Year;
+        return age;
+    }
+    public string GetAge(int year)
+    {
+        var age = DateTime.Now.Year - DateofBirth.Year;
+        return age.ToString();
+    }
+}
