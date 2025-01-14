9fbff107ff0bbc9984cc800dd3e9184227d153e7
kennylu <a29037651@yahoo.com.tw>
2025-01-14 08:53:01 +0800

    new

---
+++ aspnetcore/ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            //return View();
+            return new ViewResult() { ViewName = "ABC" };
+        }
+    }
+}
