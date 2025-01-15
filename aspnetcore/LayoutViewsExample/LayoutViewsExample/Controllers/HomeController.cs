35272dd8ab3571c39c3772265b5cae4e16a90251
kennylu <a29037651@yahoo.com.tw>
2025-01-15 14:18:05 +0800

    new one

---
+++ aspnetcore/LayoutViewsExample/LayoutViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about-company")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("contact-support")]
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
