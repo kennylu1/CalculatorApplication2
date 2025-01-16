64510e56cbb64e531b377691c8dfdfa74328f8f0
kennylu <a29037651@yahoo.com.tw>
2025-01-16 08:17:53 +0800

    加入view data

--- aspnetcore/LayoutViewsExample/LayoutViewsExample/obj/Debug/net8.0/LayoutViewsExample.AssemblyInfo.cs
+++ aspnetcore/LayoutViewsExample/LayoutViewsExample/obj/Debug/net8.0/LayoutViewsExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35272dd8ab3571c39c3772265b5cae4e16a90251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce50020ab82d7611a13b4644e48afa16108efa42")]
 [assembly: System.Reflection.AssemblyProductAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
