59d22cef819569118cf268337aa5d477562f4b83
kennylu <a29037651@yahoo.com.tw>
2025-01-16 19:41:25 +0800

    123

--- aspnetcore/PartialViewExample/PartialViewExample/obj/Debug/net8.0/PartialViewExample.AssemblyInfo.cs
+++ aspnetcore/PartialViewExample/PartialViewExample/obj/Debug/net8.0/PartialViewExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartialViewExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e2390421b782feeed16f9bb15717d2cf82adeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d69a304038dda7281201600a2f123b76f71af24")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartialViewExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartialViewExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
