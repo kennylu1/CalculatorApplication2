51af55ce44587cbe1e1e1d6327e6a650c798fa3f
kennylu <a29037651@yahoo.com.tw>
2025-01-16 10:55:41 +0800

    new

---
+++ aspnetcore/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
